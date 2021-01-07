93775873a17faabab20f2c2dbe642b388b689140
Louise <louisepeiffer@hotmail.com>
2021-01-07 12:00:12 +0100

    exo 1 et 2 finis

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,35 @@
 // Exo 1
-let jour = prompt('Un jour de la semaine')
+let jour = prompt('Un jour de la semaine').toLocaleLowerCase()
 switch (jour) {
     case "lundi":
         alert('Cours sur discord')
         break;
+    case "mardi":
+        alert('Cours sur discord')
+        break;
+    case "mercredi":
+        alert('Cours sur discord')
+        break;
+    case "jeudi":
+        alert('Cours sur discord')
+        break;  
+    case "vendredi":
+        alert('Cours sur discord')
+        break;     
     default:
         alert('Pas cours')
+        break;
+}
+
+// Exo 2
+let age = prompt('Quel âge as-tu?')
+switch (age) {
+    case "17" :
+        alert("Tu es mineur petit con")
+        break;
+    case "22" :
+        alert("Tu es majeur grand con")
+        break;
+    default:
         break;
 }
