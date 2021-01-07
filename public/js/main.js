8c0f2748c1e6cd6f960bc9f7c73d9949b021170a
Louise <louisepeiffer@hotmail.com>
2021-01-07 10:25:12 +0100

    switch exo

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+// Exo 1
+let jour = prompt('Un jour de la semaine')
+switch (jour) {
+    case "lundi":
+        alert('Cours sur discord')
+        break;
+    default:
+        alert('Pas cours')
+        break;
+}
