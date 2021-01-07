1a04f20232770a1c38d51cee5c6518975709139b
Louise <louisepeiffer@hotmail.com>
2021-01-07 15:02:27 +0100

    petites améliorations

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,21 @@
 // Exo 1
 let jour = prompt('Un jour de la semaine').toLocaleLowerCase()
 switch (jour) {
-    case "lundi":
-        alert('Cours sur discord')
+    case "lundi": case "mardi": case "mercredi": case"jeudi": case"vendredi":
+        alert(`Cours sur discord le ${jour}`)
         break;
-    case "mardi":
-        alert('Cours sur discord')
-        break;
-    case "mercredi":
-        alert('Cours sur discord')
-        break;
-    case "jeudi":
-        alert('Cours sur discord')
-        break;  
-    case "vendredi":
-        alert('Cours sur discord')
-        break;     
+    // case "mardi":
+    //     alert('Cours sur discord')
+    //     break;
+    // case "mercredi":
+    //     alert('Cours sur discord')
+    //     break;
+    // case "jeudi":
+    //     alert('Cours sur discord')
+    //     break;  
+    // case "vendredi":
+    //     alert('Cours sur discord')
+    //     break;     
     default:
         alert('Pas cours')
         break;
@@ -23,8 +23,7 @@ switch (jour) {
 
 // Exo 2
 let age = prompt('Quel âge as-tu?')
-let rep = (age<18)
-switch (rep) {
+switch (age<18) {
     case true :
         alert("Tu es mineur petit con")
         break;
